325178d06396f95db9f7fe9b4bb1fc70f5342c3c
StevenPetrik <steven.petrik@student.northampton.edu>
2025-08-29 17:40:52 +0000

    Finished!

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,13 @@
-// YOUR NAME HERE
+// Steven Petrik
 #include <iostream>
 using namespace std;
 
 int main () {
-
+    int x;
+    int y;
+    cout<<"Enter first integer: ";
+    cin>>x;
+    cout<<"Enter second integer: ";
+    cin>>y;
+    cout<<"\nThe sum is "<< (x + y) <<"."<< endl;
 }
